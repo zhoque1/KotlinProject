d082e5c4a3c8773e53410acd53041db2e3e5ef1a
zhoque <zhoque@costar.com>
2025-04-24 21:21:24 -0700

    Added bottom navigation

--- composeApp/src/commonMain/kotlin/org/demo/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/demo/project/App.kt
@@ -41,9 +41,9 @@ import org.koin.compose.viewmodel.koinViewModel
 fun App() {
     MaterialTheme {
         val navController = rememberNavController()
-//        BookNav(navController = navController, startDestination = Route.BookGraph)
+        BookNav(navController = navController, startDestination = Route.BookGraph)
 //        BottomNav(navController = navController, startDestination = Route.BookGraph)
-        RootNav(navController = navController, startDestination = Route.BookGraph)
+//        RootNav(navController = navController, startDestination = Route.BookGraph)
 //        NavComposeApp()
 
     }
